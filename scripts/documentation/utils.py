625ffd7fb433610f0ef8ec3da5850803d59c2f1d
Lefteris Karapetsas <lefteris@refu.co>
2013-06-01 23:20:57 +0200

    Removing files that should have not been here
    - The removed files belong to to the documentation creation
      which is not part of the "public" repository.
    
    - They were accidentally comitted at some point and are getting removed now
    
    - Documentation creation may be reintroduced to the public repository
      in the future

--- scripts/documentation/utils.py
+++
@@ -1,9 +0,0 @@
-import os, errno
-
-# Remove a file if it exists
-def safeRemove(filename):
-    try:
-        os.remove(filename)
-    except OSError as e:
-        if e.errno != errno.ENOENT: # errno.ENOENT = no such file or directory
-            raise # re-raise exception if a different error occured
