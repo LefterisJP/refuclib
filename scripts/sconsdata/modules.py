997e99422fe2159ee46ba20f347b22d128d39a6c
Lefteris Karapetsas <lefteris@refu.co>
2013-06-21 08:42:20 +0200

    Removing xml module from compiling for now, it is broken anyway. Also some in between changes in modules.py in preparation of next commit

--- scripts/sconsdata/modules.py
+++ scripts/sconsdata/modules.py
@@ -1,21 +1,21 @@
 import os
 from code_gen import code_gen
 
+class TemplateSourcesError(Exception):
+    def __init__(self, module_name, sources_num):
+        self.module_name = module_name
+        self.sources_num = sources_num
+    def __str__(self):
+        return ("Template module \"{}\" contains {} source files which is"
+        "more than the limitation of 1".format(self.module_name,
+                                               str(self.sources_num)))
+
 class Module:
     """Represents a module of the code with its names, its list of sources
        its #ifdef macro and a list of other modules it depends on
     """
-    name = ""
-    sources = []
-    win32_sources = []
-    linux_source = []
-    macro = []
-    dependencies = []
-    can_generate = ""
-    gen_name_sub = []
-    extra_generated = False
     def __init__(self,name,sources=[], macro="", win32_sources=[],
-                 linux_sources=[], dependencies=[], can_generate="",
+                 linux_sources=[], dependencies=[], has_template="",
                  gen_name_sub=[]):
             self.name = name
             self.sources = sources
@@ -23,79 +23,92 @@ class Module:
             self.win32_sources = win32_sources
             self.linux_sources = linux_sources
             self.dependencies = dependencies
-            self.can_generate = can_generate
+            self.has_template = has_template
             self.gen_name_sub = gen_name_sub
             self.extra_generated = False
 
     def add(self, sources, env, targetSystem, arg_env,
-            deps=None, check=True):
+            deps=[], check=True):
         """Addition of a module's sources including
            additional check for dependencies and 
            recursive addition of said dependencies
 
            -sources: The list of sources for compilation
            -env: The Scons environment variable
-v           -targetSystem: The OS name
+           -targetSystem: The OS name
            -deps: A list of current modules to build -- helps in
             avoiding duplication
            -check: A flag to determine whether we should check for
             dependencies or not
         """
-        sources.extend(self.sources)
         env.Append(CPPDEFINES= {self.macro:None})
-        if check:
-            needed = set(self.dependencies)-set(deps)
-            for m in modules:
-                    if m.name in needed:
-                            deps.append(m.name)
-                            m.add(sources,env,targetSystem,deps)
-        if(targetSystem == 'Windows'):
-                sources.extend(self.win32_sources)
-        else:
-                sources.extend(self.linux_sources)
-        # from here and under we are checking if more sources should be 
-        # generated
-        if(self.can_generate != ""):
-            #if any additional data types are requested for this module
-            if arg_env[self.can_generate] != []:
-                #take the name from the first source for now
+        #if this is a template module
+        if(self.has_template != ""):
+            #if any data types are requested for this module
+            if arg_env[self.has_template] != []:
+                #take the name from the first (and only) source
                 fullname = self.sources[0]
+                #for template there should be only one source
+                if len(self.sources) != 1:
+                    raise TemplateSourcesError(self.name,
+                                               len(self.sources))
                 name = os.path.basename(fullname)
                 #get rid of .c
                 name = name[:len(name)-2]
                 parent_dir = os.path.dirname(fullname)
                 root = arg_env['REFU_DIR']
                 #for every type of data given for the module
-                for d_type in arg_env[self.can_generate]:
-                    #generate the requested extra sources for each
-                    code_gen(name, parent_dir, root,
-                                   d_type, self.gen_name_sub)
-                    #and of course add it to the sources to compile
-                    sources.append(
-                        os.path.join(parent_dir, name + d_type + ".c")
-                    )
-                #add lines to rftokens.h
-                #TODO
+                for d_type in arg_env[self.has_template]:
+                    if d_type != "GENERIC":
+                        #generate the requested extra sources for each
+                        code_gen(name, parent_dir, root,
+                                 d_type, self.gen_name_sub)
+                        #and of course add it to the sources to compile
+                        sources.append(
+                            os.path.join(parent_dir,
+                                         name + "_" + d_type + ".c")
+                        )
+                    else:
+                        #the GENERIC type should be the template itself
+                        sources.append(self.sources)
+                #TODO: add lines to rftokens.h
                 #create the extra include file
                 create_includes(name, parent_dir,
-                                root,  arg_env[self.can_generate])
+                                root,  arg_env[self.has_template])
                 #add the appropriate define to include
                 #the extra headers for the source we generated
                 env.Append(CPPDEFINES= {self.macro+"_EXTRA": None})
                 #also mark this module as needing the extra macro defined
                 self.extra_generated = True
+        else:
+            #else if this is a normal - non template module
+            sources.extend(self.sources)
+            if(targetSystem == 'Windows'):
+                sources.extend(self.win32_sources)
+            else:
+                sources.extend(self.linux_sources)
+            if check:
+                needed = set(self.dependencies)-set(deps)
+                for m in modules:
+                    if m.name in needed:
+                        deps.append(m.name)
+                        m.add(sources,env,targetSystem,deps)
 
                                             
 
 def create_includes(name, parent_dir, root, types_list):
+    """
+        Creates the include files needed for each of the generated
+        source files.
+    """
     f = open(
         os.path.join(root, "include", parent_dir, name + "_extra.h"),
         "w")
     for type_symbol in types_list:
-        f.write("#include<{}/{}_decl.h>\n".format(parent_dir,
-                                                  name + type_symbol))
-        f.write("#include<{}/{}.h>\n".format(parent_dir,
-                                                  name + type_symbol))
+        f.write("#include <{}/{}_decl.h>\n".format(parent_dir,
+                                                   name + "_" + type_symbol))
+        f.write("#include <{}/{}.h>\n".format(parent_dir,
+                                              name + "_" + type_symbol))
     f.close()
 
 def setup_modules(arg_env, env, targetSystem):
@@ -104,7 +117,7 @@ def setup_modules(arg_env, env, targetSystem):
        the required source file and dependencies
     """
     if 'ALL' not in arg_env['REFU_MODULES']:
-        # add the core modules 
+        # add the core and system modules 
         if 'CORE' not in arg_env['REFU_MODULES']:
             arg_env['REFU_MODULES'].append('CORE')
             if 'SYSTEM' not in arg_env['REFU_MODULES']:
@@ -156,35 +169,28 @@ dependencies	= ['STRING'])
 )
 
 modules.append(
-Module("LINKED_LIST",
-['Data_Structures/linkedlist.c'],
-macro = "RF_MODULE_DS_LINKED_LIST",
-can_generate = "LINKED_LIST",
-gen_name_sub = ["RF_LL_Node",
-                "RF_LinkedList",
-                "rfLinkedList",
-                "RF_LINKEDLIST",
-                "linkedlist"
+Module("LIST",
+['Data_Structures/list.c'],
+macro = "RF_MODULE_LIST",
+has_template = "LIST",
+gen_name_sub = ["RF_LNode",
+                "RF_List",
+                "rfList",
+                "RF_LIST",
+                "list"
                ]
 ))
 
 modules.append(
-Module("LIST",
-['Data_Structures/list.c'],
-macro			= "RF_MODULE_DS_LIST")
-)
-
-modules.append(
-Module("ARRAY",
-['Data_Structures/array.c'],
-macro			= "RF_MODULE_DS_ARRAY",
-dependencies 	= ['BINARY_ARRAY'])
+Module("DYNAMIC_ARRAY",
+['Data_Structures/dynamicarray.c'],
+macro			= "RF_MODULE_DYNAMICARRAY")
 )
 
 modules.append(
 Module("BINARY_ARRAY",
 ['Data_Structures/binaryarray.c'],
-macro			= "RF_MODULE_DS_BARRAY")
+macro			= "RF_MODULE_BINARYARRAY")
 )
 
 modules.append(
@@ -202,7 +208,7 @@ macro			= "RF_MODULE_TIME_DATE")
 
 modules.append(
 Module("XML",
-['Data_Formats/xml.c', 'Data_Formats/xmltag.c', 'Data_Formats/commonp.c'],
+#['Data_Formats/xml.c', 'Data_Formats/xmltag.c', 'Data_Formats/commonp.c'],
 macro			= "RF_MODULE_DF_XML",
 dependencies	= ['TEXTFILE', 'String', 'List'])
 )
