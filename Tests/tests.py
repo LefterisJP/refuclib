a9c7d14497a4706f506f70c2e35708aecbbd9712
Lefteris Karapetsas <lefteris@refu.co>
2013-09-12 00:47:52 +0200

    Tests are now class objects in python
    - Made the way tests are created and maintained a lot more organized by having each test being
      an object of type test.
    
    - Object files are cleared AFTER tests are finished
    
    - Did some flake8 around the code

--- Tests/tests.py
+++ Tests/tests.py
@@ -1,130 +1,17 @@
-import subprocess
 from subprocess import CalledProcessError
 import os
 import shutil
 import time
 import platform
-import sys
 
-from testscompile import compileTest
+from tests_module import tests_list
 from output import (print_nonl, report, TestError, TestsFail, TestCompileError)
 from log import check_library_log
-
-def copy_sources_for_list():
-    """ Copies the extra object generated source for the list test """
-    try:
-        os.rename(os.path.join("..", "src", "Data_Structures",
-                               "list_test_object.c"),
-                  os.path.join("Data_Structures", "list_test_object.c"))
-    except:
-        print_nonl("During preparation of the list test, could not copy "
-                   "the generated sources")
-
-def cleanup_for_list():
-    """ Cleansup the copied file for the list test """
-    try:
-        os.remove(os.path.join("Data_Structures", "list_test_object.c"))
-    except:
-        pass
-
-tests_dict = { 
-    "dynamicarray": {
-        "sources": [os.path.join("Data_Structures", "dynamicarray.c")],
-        "root": "Data_Structures"
-    },
-    "hashmap": {
-        "sources": [os.path.join("Data_Structures", "hashmap.c")],
-        "root": "Data_Structures"
-    },
-    "intrusive_list": {
-        "sources": [os.path.join("Data_Structures", "intrusive_list.c")],
-        "root": "Data_Structures"
-    },
-    "list": {
-        "sources": [os.path.join("Data_Structures", "list.c"),
-                    os.path.join("Data_Structures", "list_test_object.c"),
-                    os.path.join("ExtraObjects", "test.c"),
-                ],
-        "root": "Data_Structures",
-        "action_before_compile": copy_sources_for_list,
-        "cleanup_after_test": cleanup_for_list
-    },
-
-    "string_accessor": {
-        "sources": [os.path.join("String", "string_accessor.c")],
-        "root": "String"
-    },
-    "string_additions": {
-        "sources": [os.path.join("String", "string_additions.c")],
-        "root": "String"
-    },
-    "string_conversion": {
-        "sources": [os.path.join("String", "string_conversion.c")],
-        "root": "String"
-    },
-    "string_copying": {
-        "sources": [os.path.join("String", "string_copying.c")],
-        "root": "String"
-    },
-    "string_filedescriptors": {
-        "sources": [os.path.join("String", "string_filedescriptors.c")],
-        "root": "String"
-    },
-    "string_format": {
-        "sources": [os.path.join("String", "string_format.c")],
-        "root": "String"
-    },
-    "string_init": {
-        "sources": [os.path.join("String", "string_init.c")],
-        "root": "String"
-    },
-    "string_iteration": {
-        "sources": [os.path.join("String", "string_iteration.c")],
-        "root": "String"
-    },
-    "string_positionalretrieval": {
-        "sources": [os.path.join("String", "string_positionalretrieval.c")],
-        "root": "String"
-    },
-    "string_removals": {
-        "sources": [os.path.join("String", "string_removals.c")],
-        "root": "String"
-    },
-    "string_replace": {
-        "sources": [os.path.join("String", "string_replace.c")],
-        "root": "String"
-    },
-    "string_retrieval": {
-        "sources": [os.path.join("String", "string_retrieval.c")],
-        "root": "String"
-    },
-    "string_traversal": {
-        "sources": [os.path.join("String", "string_traversal.c")],
-        "root": "String"
-    },
-
-    "system_files": {
-        "sources": [os.path.join("System", "system_files.c")],
-        "root": "System"
-    },
-
-    "textfile_lineend": {
-        "sources": [os.path.join("TextFile", "textfile_lineend.c")],
-        "root": "TextFile"
-    },
-    "textfile_read": {
-        "sources": [os.path.join("TextFile", "textfile_read.c")],
-        "root": "TextFile"
-    },
-    "textfile_write": {
-        "sources": [os.path.join("TextFile", "textfile_write.c")],
-        "root": "TextFile"
-    }
-}
 
 #the time by which to delay actions after compiling so as not to have conflicts
 #after copying/deleting binaries
 delay = 0.20
+
 
 def configureLinking(dynamic, root, outName):
     """Deals with the library linking for each test"""
@@ -133,114 +20,15 @@ def configureLinking(dynamic, root, outName):
         #test  directory source so that the lib can be accessed
         if(platform.system() == 'Windows'):
             try:
-                os.remove(os.path.join(root,outName))
+                os.remove(os.path.join(root, outName))
             except:
                 pass
-            shutil.copy(os.path.join(".",outName),os.path.join(root,outName))
-
-
-def test(root, test_name, logFile, testExec, verbose, fail_fast):
-    """
-        Tests a compiled test executable and compared it to the expected
-        output
-        Arguments:
-
-        --root: The root directory of the test compared to the Tests
-                directory
-        --test_name: The name of the test in the test_dict 
-        (hence also the .expect if existing)
-        --logFile: The name of the logfile
-        --testExec: The name of the test executable
-        --verbose: A boolean flag denoting if the tests should be verbose
-                  or not
-        --fail_fast: A boolean flag denoting if the tests should fail fast
-    """
-    print_nonl("\t--> Testing \"{}\"...".format(test_name))
-    logFile.write("\t--> Testing \"{}\"...".format(test_name))
-    #try to open the expected outputfile
-    try:
-        f = open(os.path.join(root, "{}.expect".format(test_name)), 'r')
-        expected_output = True
-    except:
-        expected_output = False
-    #call the test
-    p = subprocess.Popen(os.path.join('.',testExec),
-                         cwd=root,stdout=subprocess.PIPE)
-
-    expected_stdout_lineN = 1
-    line = None
-    for line in p.stdout:
-        #read each line
-        line = line.decode("utf8");
-        line = line.replace("\r\n","\n")
-        #if it's just a position reporting line log it and continue
-        if "passed >>" in line:
-            logFile.write("\t\t\t{}\n".format(line))
-            continue
-        #if not then let's check the expected stdout line if we got one
-        if expected_output:
-            expected_stdout = f.readline()
-            if line != expected_stdout:
-                # if it's an EXPECT() macro error, report it
-                if "*ERROR*:" in line:
-                    report(logFile, "FAILED!\n\t {}".format(line))
-                    if fail_fast:
-                        raise TestError(test_name, expected_stdout_lineN)
-                # else it's an expected std output error
-                else:
-                    report(logFile, "\n\t\tTest failed at expected "
-                           "output line \"[{}]\"\n".format(
-                               str(expected_stdout_lineN)))
-                    report(logFile, "\n\t\tExpected:\n\t\t\t{}\n\t\t"
-                           "Found:\n\t\t\t{}\n".format(
-                               str(expected_stdout), str(line)))
-                raise TestError(test_name, expected_stdout_lineN)
-            expected_stdout_lineN += 1
-    #wait till the test subprocess ends
-    ret = p.poll()
-    while(ret is None):
-        ret=p.poll()
-    #success
-    if ret is 0:
-        report(logFile, "OK!\n")
-    else:
-        report(logFile, "FAILED!\n\t\tUnexpected Return Value. Test "
-               "executable probably segfaulted.")
-        if line is not None:
-            line = line.partition("[")[2]
-            line = line.partition("]")[0]
-            report(logFile,
-                   "\t\tLast line we are sure succesfully executed "
-                   "is [{}]\n".format(str(line)))
-        raise TestError(test_name, line)
-
-    return True
-
-def debugTest(root, test_name, logFile, testExec):
-    """
-        Debugs a compiled test executable by just spitting out its output
-        --root: The root directory of the test compared to the Tests directory
-        --test_name: The name of the test
-        --logFile: The name of the logfile
-        --testExec: The name of the test executable
-    """
-    report(logFile, 
-           "\t**Testing \"{}\" in Debug Mode**\n".format(test_name))
-    p = subprocess.Popen(os.path.join('.', testExec), cwd=root, 
-                             stdout=subprocess.PIPE)
-    for line in p.stdout:#for every line of the output
-        line = line.decode("utf8")
-        line = line.replace("\r\n", "\n")
-        print_nonl("\t" + line)
-        logFile.write(line)
-    #wait till the subprocess ends
-    ret = p.poll()
-    while(ret is None):
-        ret=p.poll()
+            shutil.copy(os.path.join(".", outName),
+                        os.path.join(root, outName))
 
 
 def runTests(compiler, dynamic, outName, logFile, verbose,
-             debug, tests, fail_fast, keep_exec):
+             debug, requested_tests, fail_fast, keep_exec):
     """Runs the Tests of the Refu library
 
         --compiler: The name of the compiler
@@ -254,7 +42,7 @@ def runTests(compiler, dynamic, outName, logFile, verbose,
                    be verbose about its actions or not
         --debug: A boolean flag denoting whether the tests should be in
                  debug mode (no testing or comparison) or not
-        --tests: A list of specific tests to run. If it is empty all
+        --requested_tests: A list of specific tests to run. If it is empty all
                  tests are run. If it is not then only the specified
                  ones are.
         --fail_fast: A boolean flag denoting if the testing scripts should
@@ -276,36 +64,27 @@ def runTests(compiler, dynamic, outName, logFile, verbose,
            "===> Starting Tests for the {} version of Refu library with "
            " the \"{}\" Compiler\n\n".format(libtype_str, compiler))
 
-    # clean the tests directory from object files
-    for root, dirs, files in os.walk("."):
-        for f in files:
-            if(f.endswith(('.o','.obj'))):
-                os.remove(os.path.join(root, f))
-
     # Recursively iterate all the files in the Tests directory
-    for key, value in tests_dict.iteritems():
+    for test in tests_list:
         # check to see if the user provided specific tests, and if
         # he did check to see if this is one of them
-        if(tests and key not in tests):
+        if(requested_tests and test.name not in requested_tests):
                 continue
         #if there is any action to do before compiling do it
-        if "action_before_compile" in value:
-            value["action_before_compile"]()
+        test.before_compile()
 
         # compile it
         try:
-            compileTest(key, value["sources"], dynamic, compiler, verbose,
-                            logFile)
+            test.compile_test(dynamic, compiler, verbose, logFile)
         except TestCompileError as e:
             print(e)
             if fail_fast:
-
                 raise TestsFail(
                     "Test \"{}\" failed and --fail"
-                    "-fast was requested".format(part))
+                    "-fast was requested".format(test.name))
 
             print("Continuing with the compilation of the other tests")
-            continue#if it fails go to the next test
+            continue  # if it fails go to the next test
 
         # after each compile add a small delay since the
         # subsequent copy may fail
@@ -313,61 +92,59 @@ def runTests(compiler, dynamic, outName, logFile, verbose,
         # move the executable to the specific test directory its
         #source was and if there was one there already delete it
         try:
-            os.remove(os.path.join(value["root"], testExec))
+            os.remove(os.path.join(test.root, testExec))
         except:
             pass
         os.rename(os.path.join(".", testExec),
-                  os.path.join(value["root"], testExec))
+                  os.path.join(test.root, testExec))
         #deal with the library linking for each test
-        configureLinking(dynamic, value["root"], outName)
+        configureLinking(dynamic, test.root, outName)
 
         #test it
         try:
             if(debug):
-                debugTest(value["root"], key, logFile, testExec)
+                test.debug_test(logFile, testExec)
             else:
-                test(value["root"], key, logFile, testExec,
-                     verbose, fail_fast)
-
+                test.run_test(logFile, testExec, verbose, fail_fast)
             # check the log for errors
-            check_library_log(logFile, value["root"], key)
+            check_library_log(logFile, test.root, test.name)
         except TestError as err:
             #if there was a test error print it
             print(err)
             if fail_fast:
                 raise TestsFail(
                     "Test \"{}\" failed and --fail"
-                    "-fast was requested".format(key))
+                    "-fast was requested".format(test.name))
         except CalledProcessError as err:
             print("\tThere was an error while trying to run the "
                   "compiled "
-                  "executable for test \"{}\"".format(key))
+                  "executable for test \"{}\"".format(test.name))
             print("\tThe output was:")
             print(err.output)
             logFile.write("\tTest \"{}\" failed due to "
                           "inability to run the "
-                          "executable\n".format(key))
+                          "executable\n".format(test.name))
             if fail_fast:
                 raise TestsFail(
                     "Test \"{}\" failed and --fail"
-                    "-hard was requested".format(key))
+                    "-hard was requested".format(test.name))
         finally:
             # cleaning up
-            if "cleanup_after_test" in value:
-                value["cleanup_after_test"]()
+            test.after_test()
 
             if dynamic:
                 # also if we are testing the dynamic lib,
                 # delete its local copy
                 try:
-                    os.remove(os.path.join(value["root"], outName))
+                    os.remove(os.path.join(test.root, outName))
                 except:
                     pass
             #after finishing a test delete the executable
-            if ( not keep_exec and fail_fast or 
-                (not keep_exec and len(tests) != 1)):
+            if((not keep_exec and fail_fast)
+               or
+               (not keep_exec and len(requested_tests) != 1)):
                 try:
-                    os.remove(os.path.join(value["root"], testExec))
+                    os.remove(os.path.join(test.root, testExec))
                 except:
                     pass
             else:
@@ -377,8 +154,14 @@ def runTests(compiler, dynamic, outName, logFile, verbose,
             # a better way to do this
             time.sleep(delay)
     #after all tests have concluded clean up
-    print_nonl("\n");
-    logFile.write("\n");
+    print_nonl("\n")
+    logFile.write("\n")
+
+    # clean the tests directory from object files
+    for root, dirs, files in os.walk("."):
+        for f in files:
+            if(f.endswith(('.o', '.obj'))):
+                os.remove(os.path.join(root, f))
     try:
         os.remove(os.path.join('.', testExec))
     except:
---
+++ Tests/tests_module.py
@@ -0,0 +1,308 @@
+import subprocess
+from subprocess import CalledProcessError
+import sys
+import os
+import platform
+
+from output import TestError, TestCompileError, report, print_nonl
+
+
+def copy_sources_for_list():
+    """ Copies the extra object generated source for the list test """
+    try:
+        os.rename(os.path.join("..", "src", "Data_Structures",
+                               "list_test_object.c"),
+                  os.path.join("Data_Structures", "list_test_object.c"))
+    except:
+        print_nonl("During preparation of the list test, could not copy "
+                   "the generated sources")
+
+
+def cleanup_for_list():
+    """ Cleansup the copied file for the list test """
+    try:
+        os.remove(os.path.join("Data_Structures", "list_test_object.c"))
+    except:
+        pass
+
+
+class Test:
+    """
+    Defines a single test of the library
+    """
+    def __init__(self, name, sources, root,
+                 action_before_compile=None,
+                 action_after_test=None):
+        self.name = name
+        # list of sources to compile for the test
+        self.sources = sources
+        # root directory of the test's main source
+        self.root = root
+        # if not None this is a function to run before compiling
+        self.action_before_compile = action_before_compile
+        # if not None this is a function to run after test is ran
+        self.action_after_test = action_after_test
+
+    def before_compile(self):
+        """
+        Runs a given action before the test's compilation or does nothing
+        """
+        if self.action_before_compile is not None:
+            self.action_before_compile()
+
+    def after_test(self):
+        """
+        Performs a given action after a test has concluded or does nothing
+        """
+        if self.action_after_test is not None:
+            self.action_after_test()
+
+    def compile_test(self, dynamic, compiler, verbose, logFile):
+        """Compiles a single test file from the tests depending on the compiler
+           and library linking type
+
+            --dynamic: A boolean value denoting if the linking should be done
+            with a dynamic/shared library or not (static)
+            --compiler: The name of the compiler with which to do the compiling
+            --verbose: A boolean value denoting if the output to stdout
+            should be verbose or not
+            --logFile: The name of the logfile to print the output
+        """
+        if(dynamic):
+            target = "test_shared"
+        else:
+            target = "test_static"
+
+        sources_string = "__TEST_SOURCES="
+        for s in self.sources:
+            sources_string = sources_string+s+","
+        sources_string = sources_string[:-1]
+
+        if(platform.system() == 'Windows'):
+            sconsCall = ["scons.py", target, '-Q', 'COMPILER=' + compiler,
+                         sources_string]
+        else:
+            sconsCall = "{} scons.py {} -Q COMPILER={} {}".format(
+                sys.executable, target, compiler, sources_string)
+
+        try:
+            p = subprocess.Popen(sconsCall, cwd="..", shell=True,
+                                 stdout=subprocess.PIPE)
+            for line in p.stdout:
+                if(verbose):
+                    print("\t\t"+line.decode("utf8"))
+            #wait till the subprocess ends
+            ret = p.poll()
+            while ret is None:
+                ret = p.poll()
+            if ret != 0:
+                report(logFile,
+                       "\t\tDuring compiling test {0} the {1} compiler"
+                       " returned unsucessfully with return code {2}".format(
+                           self.name, compiler, ret))
+                msg = ""
+                if p.stdout is not None:
+                    for line in p.stdout:
+                        line = line.decode("utf8")
+                        msg = msg + line
+                        report(logFile, "\t\t"+line)
+                raise TestCompileError(self.name, msg)
+
+        except CalledProcessError as err:
+            report(logFile, "\t\tThere was an error while invoking the "
+                   "compiler for test \"{0}\"".format(self.name))
+            report(logFile, "\t\tThe compiler returned [{0}] and the output "
+                   "was:".format(err.returncode))
+            raise TestCompileError(self.name, err.output)
+        #success
+        if(verbose):
+            print("\t-> Succesfully compiled Test \"{0}\"".format(self.name))
+
+    def run_test(self, logFile, testExec, verbose, fail_fast):
+        """
+            Tests a compiled test executable and compared it to the expected
+            output
+            Arguments:
+
+            --logFile: The name of the logfile
+            --testExec: The name of the test executable
+            --verbose: A boolean flag denoting if the tests should be verbose
+                      or not
+            --fail_fast: A boolean flag denoting if the tests should fail fast
+        """
+        print_nonl("\t--> Testing \"{}\"...".format(self.name))
+        logFile.write("\t--> Testing \"{}\"...".format(self.name))
+        #try to open the expected outputfile
+        try:
+            f = open(os.path.join(self.root,
+                                  "{}.expect".format(self.name)), 'r')
+            expected_output = True
+        except:
+            expected_output = False
+        #call the test
+        p = subprocess.Popen(os.path.join('.', testExec),
+                             cwd=self.root, stdout=subprocess.PIPE)
+
+        expected_stdout_lineN = 1
+        line = None
+        for line in p.stdout:
+            #read each line
+            line = line.decode("utf8")
+            line = line.replace("\r\n", "\n")
+            #if it's just a position reporting line log it and continue
+            if "passed >>" in line:
+                logFile.write("\t\t\t{}\n".format(line))
+                continue
+            #if not then let's check the expected stdout line if we got one
+            if expected_output:
+                expected_stdout = f.readline()
+                if line != expected_stdout:
+                    # if it's an EXPECT() macro error, report it
+                    if "*ERROR*:" in line:
+                        report(logFile, "FAILED!\n\t {}".format(line))
+                        if fail_fast:
+                            raise TestError(self.name, expected_stdout_lineN)
+                    # else it's an expected std output error
+                    else:
+                        report(logFile, "\n\t\tTest failed at expected "
+                               "output line \"[{}]\"\n".format(
+                                   str(expected_stdout_lineN)))
+                        report(logFile, "\n\t\tExpected:\n\t\t\t{}\n\t\t"
+                               "Found:\n\t\t\t{}\n".format(
+                                   str(expected_stdout), str(line)))
+                    raise TestError(self.name, expected_stdout_lineN)
+                expected_stdout_lineN += 1
+        #wait till the test subprocess ends
+        ret = p.poll()
+        while ret is None:
+            ret = p.poll()
+        #success
+        if ret is 0:
+            report(logFile, "OK!\n")
+        else:
+            report(logFile, "FAILED!\n\t\tUnexpected Return Value. Test "
+                   "executable probably segfaulted.")
+            if line is not None:
+                line = line.partition("[")[2]
+                line = line.partition("]")[0]
+                report(logFile,
+                       "\t\tLast line we are sure succesfully executed "
+                       "is [{}]\n".format(str(line)))
+            raise TestError(self.name, line)
+
+        return True
+
+    def debug_test(self, logFile, testExec):
+        """
+            Debugs a compiled test executable by just spitting out its output
+
+            --logFile: The name of the logfile
+            --testExec: The name of the test executable
+        """
+        report(logFile,
+               "\t**Testing \"{}\" in Debug Mode**\n".format(self.name))
+        p = subprocess.Popen(os.path.join('.', testExec), cwd=self.root,
+                             stdout=subprocess.PIPE)
+        for line in p.stdout:  # for every line of the output
+            line = line.decode("utf8")
+            line = line.replace("\r\n", "\n")
+            print_nonl("\t" + line)
+            logFile.write(line)
+        #wait till the subprocess ends
+        ret = p.poll()
+        while ret is None:
+            ret = p.poll()
+
+
+# The list of tests. If implementing more tests, add them here
+tests_list = [
+    Test("dynamicarray",
+         [os.path.join("Data_Structures", "dynamicarray.c")],
+         "Data_Structures"),
+
+    Test("hashmap",
+         [os.path.join("Data_Structures", "hashmap.c")],
+         "Data_Structures"),
+
+    Test("intrusive_list",
+         [os.path.join("Data_Structures", "intrusive_list.c")],
+         "Data_Structures"),
+
+    Test("list",
+         [
+             os.path.join("Data_Structures", "list.c"),
+             os.path.join("Data_Structures", "list_test_object.c"),
+             os.path.join("ExtraObjects", "test.c")
+         ],
+         "Data_Structures",
+         copy_sources_for_list,
+         cleanup_for_list),
+
+    Test("string_accessor",
+         [os.path.join("String", "string_accessor.c")],
+         "String"),
+
+    Test("string_additions",
+         [os.path.join("String", "string_additions.c")],
+         "String"),
+
+    Test("string_conversion",
+         [os.path.join("String", "string_conversion.c")],
+         "String"),
+
+    Test("string_copying",
+         [os.path.join("String", "string_copying.c")],
+         "String"),
+
+    Test("string_filedescriptors",
+         [os.path.join("String", "string_filedescriptors.c")],
+         "String"),
+
+    Test("string_format",
+         [os.path.join("String", "string_format.c")],
+         "String"),
+
+    Test("string_init",
+         [os.path.join("String", "string_init.c")],
+         "String"),
+
+    Test("string_iteration",
+         [os.path.join("String", "string_iteration.c")],
+         "String"),
+
+    Test("string_positionalretrieval",
+         [os.path.join("String", "string_positionalretrieval.c")],
+         "String"),
+
+    Test("string_removals",
+         [os.path.join("String", "string_removals.c")],
+         "String"),
+
+    Test("string_replace",
+         [os.path.join("String", "string_replace.c")],
+         "String"),
+
+    Test("string_retrieval",
+         [os.path.join("String", "string_retrieval.c")],
+         "String"),
+
+    Test("string_traversal",
+         [os.path.join("String", "string_traversal.c")],
+         "String"),
+
+    Test("system_files",
+         [os.path.join("System", "system_files.c")],
+         "System"),
+
+    Test("textfile_lineend",
+         [os.path.join("TextFile", "textfile_lineend.c")],
+         "TextFile"),
+
+    Test("textfile_read",
+         [os.path.join("TextFile", "textfile_read.c")],
+         "TextFile"),
+
+    Test("textfile_write",
+         [os.path.join("TextFile", "textfile_write.c")],
+         "TextFile")
+]
