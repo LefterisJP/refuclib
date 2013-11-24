969a69190e37e0607df1f8055c88b97e4899669d
Lefteris Karapetsas <lefteris@refu.co>
2013-11-24 11:49:44 +0100

    Deleting the examples directory. Even when documentation gets up to date, unit tests should be enough for example usage

--- Examples/Preprocessor/Counting_Arguments/example.c
+++
@@ -1,3 +0,0 @@
-RF_NARG()//returns 0
-RF_NARG()//returns 0
-RF_NARG(foo,1,3,"lorem ipsum")//returns 4
--- Examples/Preprocessor/Default_Arguments/example.h
+++
@@ -1,8 +0,0 @@
-//Here we want to declare a function that takes 3 arguments
-// and the last 2 of them can have default values
-RF_DECLARE_DFUNC3(float, //function will return float
-				  test_func,//function name
-				  2,//2 out of 3 arguments will have default values
-				  int,//first argument type
-				  int,//second argument type 
-				  float)//fourth argument type 
